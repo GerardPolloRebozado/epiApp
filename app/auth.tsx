8829fe0209b2667ce4758bba7005521761132a0f
gerardPolloRebozado <gerardit4@gmail.com>
2025-01-16 16:10:43 +0100

    feat: :sparkles: Added week on top of activities and project button and moved week calculation logic to another function

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,27 +1,31 @@
 import useSession from "@/hooks/ctx";
-import { Redirect, useNavigation } from "expo-router";
+import {useLocalSearchParams, useNavigation, useRouter} from "expo-router";
 import CookieManager from '@react-native-cookies/cookies';
 import WebView from "react-native-webview";
-import { useEffect } from "react";
+import {useEffect} from "react";
 
 export default function Auth() {
     const {session, signIn} = useSession();
     const navigation = useNavigation();
+    const router = useRouter();
+    const params = useLocalSearchParams<{ cloudflare?: string }>();
 
-    navigation.setOptions({ title: 'Please login the intranet' });
+    navigation.setOptions({title: params.cloudflare === 'true' ? 'Wait till cloudflare protection completes' : 'Please login'});
     if (session) {
-        return <Redirect href={'/'}/>;
+        router.push('/');
     }
 
     useEffect(() => {
-        const interval = setInterval(async () => {
-            const cookies = await CookieManager.get('https://intra.epitech.eu');
-            console.log(cookies);
-            if (cookies.user) {
-                clearInterval(interval);
-                signIn(cookies.user.value);
-            }
-        }, 4000);
+        setTimeout(() => {
+            const interval = setInterval(async () => {
+                const cookies = await CookieManager.get('https://intra.epitech.eu');
+                console.log(cookies);
+                if (cookies.user) {
+                    clearInterval(interval);
+                    signIn(cookies.user.value);
+                }
+            }, params.cloudflare === 'true' ? 1000 : 8000);
+        }, 1000);
     }, []);
 
     return (
--- utils/randomUtils.ts
+++ utils/randomUtils.ts
@@ -1,8 +1,20 @@
-export default function transformHours(date: string) {
+export function transformHours(date: string) {
     const [hours, minutes, seconds] = date.split(':');
     if (parseInt(hours) > 0) {
         return hours + ':' + minutes + (parseInt(hours) > 1 ? ' hours' : ' hour')
     } else {
         return minutes + ':' + seconds + ' minutes'
     }
+}
+
+export function weekCalculator(week?: number) {
+    let date = new Date()
+    date.setDate(date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1))
+    if (week && week !== 0) {
+        date.setDate(date.getDate() + (week * 7))
+    }
+    const start = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+    date.setDate(date.getDate() + 6)
+    const end = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+    return { start, end }
 }

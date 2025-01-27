6aad4a5569cd01c44e13de2022a07284d9508a71
gerardPolloRebozado <gerardit4@gmail.com>
2025-01-27 13:39:38 +0100

    feat: :sparkles: Profile page now has a chart with log time

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { RefreshControl, StyleSheet } from 'react-native';
+import { RefreshControl, StyleSheet, useColorScheme } from 'react-native';
 import useSession from "@/hooks/ctx";
 import { useEffect, useState } from "react";
 import { MarksType, NotesType, UserType } from "@/types";
@@ -8,18 +8,21 @@ import { Accordion, Avatar, Button, Card, ListItem, Paragraph, ScrollView, Separ
 import { Settings } from "@tamagui/lucide-icons";
 import { fakeMarks, fakeUser } from "@/utils/fakeData";
 import { useRouter } from "expo-router";
+import { BarChart, barDataItem } from "react-native-gifted-charts";
 
 export default function Profile() {
-    const {signOut, session} = useSession();
+    const { signOut, session } = useSession();
     const [isLoading, setIsLoading] = useState(false);
+    let colorScheme = useColorScheme()
     const [reload, setReload] = useState(true)
     const [user, setUser] = useState<UserType | null>(null);
     const [image, setImage] = useState<string>("error");
     const [marks, setMarks] = useState<MarksType | null>(null)
+    const [logTime, setLogTime] = useState<barDataItem[] | null>(null)
     const router = useRouter();
 
     useEffect(() => {
-        async function getData() {
+        (async function () {
             if (!session) {
                 console.log('No session available');
                 setIsLoading(false);
@@ -59,72 +62,98 @@ export default function Profile() {
                     });
                     setMarks(data)
                 }
+                setIsLoading(false)
             } catch (error) {
                 console.error('Fetch error:', error);
-            } finally {
-                setIsLoading(false);
+                signOut();
             }
-        }
-
-        getData()
-        async function getLogTime() {
+        }());
+        (async function () {
             if (!session || session == 'guest') return
-            const logReq = await fetchtLogtime({session});
-        }
-        getLogTime()
+            const logReq = await fetchtLogtime({ session });
+            if (logReq.status != 200) {
+                signOut();
+            }
+            const logBody = await logReq.json();
+            const current_week = await logBody.splice(-5);
+            const formatLogTime: barDataItem[] = await current_week.map((log: Array<any>) => {
+                const value = log[1] / 3600;
+                const label = new Date(log[0] * 1000).toLocaleDateString('en-US', {weekday: 'long'}).slice(0, 3);
+                return { value, label };
+            });
+            setLogTime(formatLogTime);
+        }());
     }, [session, reload]);
 
     if (isLoading || !user || !marks) {
         return (<SafeAreaView style={styles.container}>
-            <Spinner size="large"/>
+            <Spinner size="large" />
         </SafeAreaView>);
     }
 
     return (<SafeAreaView style={styles.container}>
         <XStack justifyContent={"flex-end"} alignItems={"flex-end"} width={"100%"} marginTop={"$2"} marginRight={"$1"}>
-            <Settings onPress={() => router.push('/settings')}/>
+            <Settings onPress={() => router.push('/settings')} />
         </XStack>
-        <Card bordered padded>
-            <Card.Footer alignItems={"center"} gap={"$3"}>
-                <Avatar radiused={true} size={"$8"}>
-                    <Avatar.Image src={image}/>
-                </Avatar>
-                <YStack>
-                    <Paragraph size={"$6"}>{user.title}</Paragraph>
-                    <XStack justifyContent={"space-around"} gap={"$2"}>
-                        <Paragraph size={"$3.5"}>{user.studentyear} year</Paragraph>
-                        <Paragraph size={"$3.5"}>{user.credits} credits</Paragraph>
-                        <Paragraph size={"$3.5"}>{user.gpa[0].gpa} GPA</Paragraph>
-                    </XStack>
-                </YStack>
-            </Card.Footer>
-        </Card>
-        <YStack maxHeight={"70%"} gap={"$2"}>
-            <Paragraph size="$5" textAlign={"center"}>Marks</Paragraph>
-            <ScrollView refreshControl={<RefreshControl refreshing={isLoading} onRefresh={() => setReload(!reload)}/>} gap={"$4"}>
-                <Accordion type={"multiple"} collapsable marginTop={"4"}>
-                    {marks.modules.map((module) => (
-                        <Accordion.Item value={module.codemodule} key={module.codemodule + module.codeinstance}>
-                            <Accordion.Trigger flexDirection="row" justifyContent="space-between">
-                                <Paragraph>{module.title} <Text color={module.grade !== "A" && module.grade !== "B" && module.grade !== '-' ? "red" : "green"}>{module.grade !== '-' ? `- ${module.grade}` : ""}</Text></Paragraph>
-                            </Accordion.Trigger>
-                            <Accordion.HeightAnimator animation="slow">
-                                <Accordion.Content animation="slow" exitStyle={{opacity: 0}}>
-                                    <YStack>
-                                        {module.notes.map((note: NotesType) => (
-                                            <YStack key={note.final_note + note.title}>
-                                                <ListItem><Paragraph>{note.title}</Paragraph> <Paragraph>{note.final_note.toString(10)}</Paragraph></ListItem>
-                                                <Separator/>
-                                            </YStack>
-                                        ))}
-                                    </YStack>
-                                </Accordion.Content>
-                            </Accordion.HeightAnimator>
-                        </Accordion.Item>
-                    ))}
-                </Accordion>
-            </ScrollView>
-            <Button onPress={signOut} bordered>Logout</Button>
+        <YStack gap={'20'} width={'90%'}>
+            <Card bordered padded>
+                <Card.Footer alignItems={"center"} gap={"$3"}>
+                    <Avatar radiused={true} size={"$8"}>
+                        <Avatar.Image src={image} />
+                    </Avatar>
+                    <YStack>
+                        <Paragraph size={"$6"}>{user.title}</Paragraph>
+                        <XStack justifyContent={"space-around"} gap={"$2"}>
+                            <Paragraph size={"$3.5"}>{user.studentyear} year</Paragraph>
+                            <Paragraph size={"$3.5"}>{user.credits} credits</Paragraph>
+                            <Paragraph size={"$3.5"}>{user.gpa[0].gpa} GPA</Paragraph>
+                        </XStack>
+                    </YStack>
+                </Card.Footer>
+            </Card>
+            {logTime && (
+            <Card bordered padded>
+                <Card.Header>
+                    <Paragraph size={"$5"}>Log time</Paragraph>
+                </Card.Header>
+                <Card.Footer>
+                    <BarChart
+                    data={logTime}
+                    barBorderRadius={10}
+                    frontColor={"#0091d3"}
+                    xAxisLabelTextStyle={{ color: colorScheme === 'dark' ? '#FFFFFF' : '#000000' }}
+                    yAxisTextStyle={{ color: colorScheme === 'dark' ? '#FFFFFF' : '#000000' }}
+                    />
+                </Card.Footer>
+            </Card>
+            )}
+            <YStack maxHeight={"70%"} gap={"$2"}>
+                <Paragraph size="$5" textAlign={"center"}>Marks</Paragraph>
+                <ScrollView refreshControl={<RefreshControl refreshing={isLoading} onRefresh={() => setReload(!reload)} />} gap={"$4"}>
+                    <Accordion type={"multiple"} collapsable marginTop={"4"}>
+                        {marks.modules.map((module) => (
+                            <Accordion.Item value={module.codemodule} key={module.codemodule + module.codeinstance}>
+                                <Accordion.Trigger flexDirection="row" justifyContent="space-between">
+                                    <Paragraph>{module.title} <Text color={module.grade !== "A" && module.grade !== "B" && module.grade !== '-' ? "red" : "green"}>{module.grade !== '-' ? `- ${module.grade}` : ""}</Text></Paragraph>
+                                </Accordion.Trigger>
+                                <Accordion.HeightAnimator animation="slow">
+                                    <Accordion.Content animation="slow" exitStyle={{ opacity: 0 }}>
+                                        <YStack>
+                                            {module.notes.map((note: NotesType) => (
+                                                <YStack key={note.final_note + note.title}>
+                                                    <ListItem><Paragraph>{note.title}</Paragraph> <Paragraph>{note.final_note.toString(10)}</Paragraph></ListItem>
+                                                    <Separator />
+                                                </YStack>
+                                            ))}
+                                        </YStack>
+                                    </Accordion.Content>
+                                </Accordion.HeightAnimator>
+                            </Accordion.Item>
+                        ))}
+                    </Accordion>
+                </ScrollView>
+                <Button onPress={signOut} bordered>Logout</Button>
+            </YStack>
         </YStack>
     </SafeAreaView>);
 }
@@ -133,5 +162,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
+        gap: 20,
     },
 });
